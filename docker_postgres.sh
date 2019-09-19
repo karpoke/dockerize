10e24efc5a0732413051c889603435b878c5cc24
karpoke <karpoke+github@gmail.com>
2019-09-20 01:39:13 +0200

    aAdded postgres

---
+++ docker_postgres.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# https://hub.docker.com/_/postgres/
+
+# install
+# docker pull postgres:11  # latest
+
+# https://pythonspeed.com/articles/faster-db-tests/
+# -c fsync=off
+
+mkdir -p "$HOME/docker/volumes"
+
+# acceso mediante IP y socket
+#docker inspect postgres >/dev/null 2>&1 || docker run \
+docker ps | grep -q postgres-docker || docker run \
+    --rm \
+    --name docker-postgres \
+    -h postgres \
+    -d \
+    -p 5432:5432 \
+    -v "$HOME/docker/volumes/postgres:/var/lib/postgresql/data" \
+    -v /var/run/postgresql:/var/run/postgresql \
+    postgres -c fsync=off
