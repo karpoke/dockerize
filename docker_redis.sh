4cb52ec0042bfb045ee0fe28b9fb6e5dd6efb8bf
Nacho Cano <nacho.cano@roiback.com>
2019-09-20 11:44:30 +0200

    Added redis

---
+++ docker_redis.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# https://hub.docker.com/_/redis/
+
+# install
+# docker pull redis:4.0.6  # not latest
+
+# all redis.conf params can be passed as arguments
+
+mkdir -p $HOME/docker/volumnes
+
+docker ps | grep -q docker-redis || docker run \
+    --rm \
+    --name docker-redis \
+    -h redis \
+    -d \
+    -p 6379:6379 \
+    -v $HOME/docker/volumes/redis:/data \
+    redis redis-server --databases 4096
