a35aaa5aec0323bb693904995eafbee88baf7cc9
karpoke <karpoke+github@gmail.com>
2019-10-04 13:32:21 +0200

    Added selenium

---
+++ docker_selenium.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# https://hub.docker.com/r/selenium/standalone-chrome
+# https://github.com/SeleniumHQ/docker-selenium
+
+# install
+# docker pull selenium/standalone-chrome
+
+docker run \
+    --rm \
+    --name docker-selenium \
+    -h selenium \
+    -d \
+    -p 4444:4444 \
+    -v /dev/shm:/dev/shm \
+    -e SCREEN_WIDTH=1366 \
+    -e SCREEN_HEIGHT=768 \
+    -e SCREEN_DEPTH=24 \
+    -e SCREEN_DPI=74 \
+    selenium/standalone-chrome
