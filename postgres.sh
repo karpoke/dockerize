775a9e854dae08c49dc29aacb5eea78e2e0d9d6c
karpoke <karpoke+github@gmail.com>
2021-11-04 09:06:15 +0100

    Use PostgreSQL 14 docker image

--- postgres.sh
+++ postgres.sh
@@ -3,7 +3,7 @@
 # https://hub.docker.com/_/postgres/
 
 # install
-# docker pull postgres:12  # latest
+# docker pull postgres:14  # latest
 
 # https://pythonspeed.com/articles/faster-db-tests/
 # -c fsync=off
@@ -21,4 +21,4 @@ mkdir -p "$HOME/docker/volumes"
     -p 5432:5432 \
     -v "$HOME/docker/volumes/postgres:/var/lib/postgresql/data" \
     -v /var/run/postgresql:/var/run/postgresql \
-    postgres:12 -c fsync=off
+    postgres:14 -c fsync=off
